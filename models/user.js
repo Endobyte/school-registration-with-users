261a08e5c8c201745c6a037f6bd9fe8c310fc2cc
crl2 <crl2@fresnou.org>
2023-03-16 11:33:40 -0700

    Register staff and redirect guests

--- models/user.js
+++ models/user.js
@@ -15,12 +15,25 @@ module.exports = (sequelize, DataTypes) => {
         as: 'student',
         foreignKey: 'user_id'
       })
+      User.hasOne(models.Staff, {
+        as: 'staff',
+        foreignKey: 'user_id'
+      })
     }
   };
   User.init({
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.STRING
+    role: DataTypes.STRING,
+    displayName: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        if (this.student) {
+          return this.student.first_name;
+        };
+        return this.staff.first_name;
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
